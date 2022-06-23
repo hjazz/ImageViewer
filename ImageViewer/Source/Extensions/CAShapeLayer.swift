7ce390c8577be92ff6de55f52db3a6a4aa2ff0c0
hjazz <hjsung@gmail.com>
2022-06-23 11:27:15 +0900

    resize triangle

--- ImageViewer/Source/Extensions/CAShapeLayer.swift
+++ ImageViewer/Source/Extensions/CAShapeLayer.swift
@@ -71,7 +71,7 @@ extension CAShapeLayer {
         let frame = CGRect(origin: CGPoint.zero, size: CGSize(width: diameter, height: diameter))
         circle.frame = frame
         let circlePath   = UIBezierPath(ovalIn: frame)
-        let trianglePath = UIBezierPath.equilateralTriangle(diameter / 3, shiftBy: CGPoint(x: diameter / 4, y: diameter / 5))
+        let trianglePath = UIBezierPath.equilateralTriangle(diameter / 3, shiftBy: CGPoint(x: diameter / 3, y: diameter / 4))
 
         circlePath.append(trianglePath)
         circle.path = circlePath.cgPath
